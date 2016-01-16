b0be8ff71d4839ae2c6ac1eccf28b6d45f282002
kazanov <bacbok@gmail.com>
2016-01-16 15:21:57 +0300

    Add notes edit and delete

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -25,15 +25,20 @@ class NotesController < ApplicationController
 	end
 
 	def edit
-		
+
 	end
 
 	def update
-		
+		if @note.update(note_params)
+			redirect_to @note
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-		
+		@note.destroy
+		redirect_to @note
 	end
 
 private
